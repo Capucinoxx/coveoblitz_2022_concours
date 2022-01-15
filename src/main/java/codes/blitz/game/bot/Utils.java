564177960d0c36a4fc873787e9d6735e31dc514b
dovakin7 <xavier7.lemieux@hotmail.com>
2022-01-14 19:12:27 -0500

    diamon map + held diamond

--- src/main/java/codes/blitz/game/bot/Utils.java
+++ src/main/java/codes/blitz/game/bot/Utils.java
@@ -2,35 +2,42 @@ package codes.blitz.game.bot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 
-import codes.blitz.game.message.game.GameMap;
-import codes.blitz.game.message.game.GameMessage;
-import codes.blitz.game.message.game.Position;
-import codes.blitz.game.message.game.Team;
-import codes.blitz.game.message.game.TileType;
-import codes.blitz.game.message.game.UnitAction;
-import codes.blitz.game.message.game.UnitActionType;
+import codes.blitz.game.message.game.*;
 
 public class Utils {
 
     public GameMessage m_message;
+    public Map<Position, Integer> DiamondMap;
 
+    public void CreateDiamondMap()
+    {
+        m_message.map().diamonds().forEach((diamond) -> {
+            DiamondMap.put(diamond.position(), diamond.points());
+        });
+    }
 
     public void SetMap(GameMessage message)
     {
         m_message = message;
     }
 
-    public List findPlayerPosition()
-    {
-        for (m_message.teams() : )
-    }
-
-    public List findAllyPosition()
-    {
-
+    public void findHeldDiamond() {
+        Map.Entry<Position, Integer> entry;
+        ArrayList<Position> unitsPosition = findUnitsPosition();
+        ArrayList<Position> hDiamonds = new ArrayList<>();
+        DiamondMap.forEach((pos, val) -> {
+            if (val > 0) {
+                for (Position p : unitsPosition) {
+                    if (p == pos) {
+                        hDiamonds.add(pos);
+                    }
+                }
+            }
+        });
     }
 }
