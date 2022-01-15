1ef9936b7a88e5f94c8fde039146849dc528898a
TahaLazreg <tlazreg1@gmail.com>
2022-01-15 10:27:59 -0500

    Fix where to drop

--- src/main/java/codes/blitz/game/bot/Utils.java
+++ src/main/java/codes/blitz/game/bot/Utils.java
@@ -364,28 +364,33 @@ public class Utils {
         return minEnemyPos;
     }
 
+
     public static Position whereToDrop(Position playerPosition)
     {
         Position north = new Position(playerPosition.x(), playerPosition.y()-1);
         Position south = new Position(playerPosition.x(), playerPosition.y()+1);
         Position east = new Position(playerPosition.x()+1, playerPosition.y());
         Position west = new Position(playerPosition.x()-1, playerPosition.y());
-        if (!Bot.wallTiles.contains(north) && !Bot.wallTiles.contains(north) && !Bot.EnemyMap.containsKey(north)
+        if (!Bot.wallTiles.contains(north) && !Bot.spawnTiles.contains(north) && !Bot.EnemyMap.containsKey(north)
+                && !Bot.PlayerMap.containsValue(north)
                 && north.x() >= 0 && north.x() < Bot.m_message.map().horizontalSize() &&
                 north.y() >= 0 && north.y() < Bot.m_message.map().verticalSize())
         {
             return north;
-        } else if (!Bot.wallTiles.contains(south) && !Bot.wallTiles.contains(south) && !Bot.EnemyMap.containsKey(south)
+        } else if (!Bot.wallTiles.contains(south) && !Bot.spawnTiles.contains(south) && !Bot.EnemyMap.containsKey(south)
+                && !Bot.PlayerMap.containsValue(south)
                 && south.x() >= 0 && south.x() < Bot.m_message.map().horizontalSize() &&
                 south.y() >= 0 && south.y() < Bot.m_message.map().verticalSize())
         {
             return south;
-        } else if (!Bot.wallTiles.contains(east) && !Bot.wallTiles.contains(east) && !Bot.EnemyMap.containsKey(east)
+        } else if (!Bot.wallTiles.contains(east) && !Bot.spawnTiles.contains(east) && !Bot.EnemyMap.containsKey(east)
+                && !Bot.PlayerMap.containsValue(east)
                 && east.x() >= 0 && east.x() < Bot.m_message.map().horizontalSize() &&
                 east.y() >= 0 && east.y() < Bot.m_message.map().verticalSize())
         {
             return east;
-        } else if (!Bot.wallTiles.contains(west) && !Bot.wallTiles.contains(west) && !Bot.EnemyMap.containsKey(west)
+        } else if (!Bot.wallTiles.contains(west) && !Bot.spawnTiles.contains(west) && !Bot.EnemyMap.containsKey(west)
+                && !Bot.PlayerMap.containsValue(west)
                 && west.x() >= 0 && west.x() < Bot.m_message.map().horizontalSize() &&
                 west.y() >= 0 && west.y() < Bot.m_message.map().verticalSize())
         {
