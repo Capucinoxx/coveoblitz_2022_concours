fb1806ce0d7b178a9d216fb9f9355de588bd7c26
TahaLazreg <tlazreg1@gmail.com>
2022-01-15 13:49:06 -0500

    Update Bot.java

--- src/main/java/codes/blitz/game/bot/Bot.java
+++ src/main/java/codes/blitz/game/bot/Bot.java
@@ -87,23 +87,32 @@ public class Bot
             }
             Map<String, Position> mapNearestDiamond;
             ArrayList<Position> nearestDiamonds = Utils.findNearestDiamonds();
-            if (!currUnit.hasDiamond() && nearestDiamonds.size() > Integer.parseInt(currUnit.id())-1) {
-                tempStream = new UnitAction(UnitActionType.MOVE,
-                        currUnit.id(),
-                        nearestDiamonds.get(Integer.parseInt(currUnit.id()) - 1));
+            Position enemyDiamondPos = Utils.findEnemyPlayerWithDiamond(currUnit.position());
+            if ((!currUnit.hasDiamond() && nearestDiamonds.size() > Integer.parseInt(currUnit.id())-1) ||
+                    (!currUnit.hasDiamond() && enemyDiamondPos != null && Utils.getDistance(currUnit.position(), enemyDiamondPos) == 1 && !spawnTiles.contains(currUnit.position()))) {
+
+                if(Utils.getDistance(nearestDiamonds.get(Integer.parseInt(currUnit.id()) - 1), currUnit.position()) <
+                        Utils.getDistance(Utils.whereToDrop(enemyDiamondPos), currUnit.position()))
+                {
+                    tempStream = new UnitAction(UnitActionType.MOVE,
+                            currUnit.id(),
+                            nearestDiamonds.get(Integer.parseInt(currUnit.id()) - 1));
+                }
+                else
+                {
+                    tempStream = new UnitAction(UnitActionType.MOVE,
+                            currUnit.id(),
+                            Utils.whereToDrop(enemyDiamondPos));
+
+                }
                 allActions.add(tempStream);
                 continue;
 
             }
 
-            Position enemyDiamondPos = Utils.findEnemyPlayerWithDiamond(currUnit.position());
+
             if(!currUnit.hasDiamond() && enemyDiamondPos != null && Utils.getDistance(currUnit.position(), enemyDiamondPos) == 1 && !spawnTiles.contains(currUnit.position())) {
                 tempStream = new UnitAction(UnitActionType.ATTACK, currUnit.id(), enemyDiamondPos);
-            }
-            else if(!currUnit.hasDiamond() && enemyDiamondPos != null && Utils.getDistance(currUnit.position(), enemyDiamondPos) > 1) {
-                tempStream = new UnitAction(UnitActionType.MOVE,
-                        currUnit.id(),
-                        Utils.whereToDrop(enemyDiamondPos));
             }
 
              /*Position enemyPos = Utils.findIfEnemyAdjacent(currUnit.id());
